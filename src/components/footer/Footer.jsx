da1c4df63956f3826d46c10621ab8584f4c8e4f4
Pablo Alvarado <paalvarador@gmail.com>
2025-05-31 01:30:09 -0500

    feat: Quitando del footer el nombre del disenador y desarrollador

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,9 +7,7 @@ const Footer = () => {
   return (
     <footer className="main_footer_area">
         <div className="footer__container">
-            <Social />
             <span className="footer__copy">&#169; AB Garage Doors. All rigths reserved</span>
-            <span className="footer_developedby">Designed & Developed by <a href="https://paalvarador.github.io" target="_blank" rel="noreferrer">paalvarador</a></span>
         </div>
     </footer>
   )
