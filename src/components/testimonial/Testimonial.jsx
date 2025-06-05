1b5e727ff7c341684cbeb054edbde6aaf069830c
Pablo Alvarado <paalvarador@gmail.com>
2025-06-05 14:46:25 -0500

    feat: Quitando el boton de Leave a Review

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -13,15 +13,6 @@ const Testimonial = () => {
         Real Feedback. Real Trust.
       </span>
       <TestimonialData />
-
-      <div className="review-button">
-        <input
-          type="button"
-          onClick={() => setModalOpen(true)}
-          value="Leave a Review"
-          style={{ width: "180px", borderRadius: "10px" }}
-        />
-      </div>
     </section>
   );
 };
